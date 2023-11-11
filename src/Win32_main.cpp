8897acf0ae5a3b16f066ec561e5147dca8bf14c8
Amir Reza <arg7971@gmail.com>
2023-11-11 01:07:06 -0600

    animating gradient on back buffer

--- src/Win32_main.cpp
+++ src/Win32_main.cpp
@@ -1,6 +1,16 @@
 #include <windows.h>
-#include <iostream>
+#include <stdint.h>
 #include "WinBase.h"
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t  int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
 
 #define internal_func static // the function marked by static keyword are local and inaccessible from other files
 #define local_persist static
@@ -10,27 +20,47 @@
 global_variable bool g_appRunning;
 global_variable BITMAPINFO g_bitmapInfo;
 global_variable void *g_bitmapBuffer;
-global_variable HBITMAP g_bitmapHandle;
-global_variable HDC g_bitmapDeviceContext;
+global_variable int g_bitmapWidth;
+global_variable int g_bitmapHeight;
+global_variable int g_bytePerPixel;
+
+
+internal_func void renderGradient(float offset){
+    uint8 *pixelBitmapBytes = (uint8 *) g_bitmapBuffer;
+    int pixelRowSize = g_bitmapWidth * g_bytePerPixel; // how many bytes are in a pixel row 
+    for(int y = 0; y < g_bitmapHeight; y++){
+        
+        // uint32 *pixelRow = (uint32 *) pixelBitmapBytes; // get a row of pixels (each pixel is 32 bits)
+        uint32 *pixel = (uint32 *) pixelBitmapBytes; // get a row of pixels (each pixel is 32 bits)
+        
+        for (int x = 0; x < g_bitmapWidth; x++){
+            uint8 blue = 0xff;
+            uint8 green = (((float)y / g_bitmapHeight) * offset * 255);     
+            uint8 red =  (((float)y / g_bitmapHeight) * (1 - offset) * 255);
+
+            *pixel = (red << 16) | (green << 8) | blue; 
+            *pixel++;
+        }
+        pixelBitmapBytes += pixelRowSize;
+    }
+}
 
 
 // DIB = Device Independent Bitmap 
 internal_func void win32ResizeDIBSection(int width, int height){
-    //TODO: bullet proof this. maybe allocate first and then free? 
 
-    //free the previous DI bitmap 
-    if(g_bitmapHandle){
-        // if we had a bitmap
-        DeleteObject(g_bitmapHandle);
+    // free the bitmap buffer memory page(s)
+    if(g_bitmapBuffer){
+        VirtualFree(g_bitmapBuffer, 0, MEM_RELEASE);
     }
 
-    if(!g_bitmapDeviceContext){
-        g_bitmapDeviceContext = CreateCompatibleDC(0);
-    }
+    g_bitmapWidth = width;
+    g_bitmapHeight = height;
 
     g_bitmapInfo.bmiHeader.biSize = sizeof(g_bitmapInfo.bmiHeader);
-    g_bitmapInfo.bmiHeader.biWidth = width;
-    g_bitmapInfo.bmiHeader.biHeight = height;
+    g_bitmapInfo.bmiHeader.biWidth = g_bitmapWidth;
+    // we make this negative because we want to our coordinate system (back buffer) start at top-left corner 
+    g_bitmapInfo.bmiHeader.biHeight = -g_bitmapHeight;
     g_bitmapInfo.bmiHeader.biPlanes = 1;
     g_bitmapInfo.bmiHeader.biBitCount = 32; // 4 byte int values for rgba
     g_bitmapInfo.bmiHeader.biCompression = BI_RGB;
@@ -40,24 +70,34 @@ internal_func void win32ResizeDIBSection(int width, int height){
     g_bitmapInfo.bmiHeader.biClrUsed = 0;
     g_bitmapInfo.bmiHeader.biClrImportant = 0;
 
-
-    
-    
-    g_bitmapHandle = CreateDIBSection(
-        g_bitmapDeviceContext, 
-        &g_bitmapInfo,
-        DIB_RGB_COLORS,
-        &g_bitmapBuffer,
-        0,
-        0
+    g_bytePerPixel = 4; // 4 RGBp where p=padding required to make it aligned ti 32/64 bit processors
+    int bufferSize = (width * height) * g_bytePerPixel;
+    // we ask windows kernel to give us page(s) of memory 
+    g_bitmapBuffer = VirtualAlloc(
+        0, // start of the memory page
+        bufferSize, 
+        MEM_COMMIT, // we need the memory now and want to write to it
+        PAGE_READWRITE // access type to our page
     );
 }
 
-internal_func void win32UpdateWindow(HDC deviceContext, int x, int y, int width, int height){
+internal_func void win32UpdateWindow(HDC deviceContext,RECT *windowRect, int x, int y, int width, int height){
+    
+    int windowWidth = windowRect->right - windowRect->left;
+    int windowHeight = windowRect->bottom - windowRect->top;
+    
     StretchDIBits(
         deviceContext,
-        x, y, width, height, // window canvas
+        // original mapping from source buffer to destination window canvas
+        /*
+         x, y, width, height, // window canvas
         x, y, width, height, // source back buffer   
+        */
+
+       //TODO: i don't know why casey did this yet!
+       0, 0, g_bitmapWidth, g_bitmapHeight,
+       0, 0, windowWidth, windowHeight,
+
         g_bitmapBuffer,
         &g_bitmapInfo,
         DIB_RGB_COLORS,
@@ -72,13 +112,15 @@ LRESULT CALLBACK mainWindowCallback(HWND window,UINT message, WPARAM wParam, LPA
 
     switch(message){
         case WM_PAINT:{
+            RECT canvas;
             PAINTSTRUCT paintStruct; // we store the new information about our window in this struct
+            GetClientRect(window, &canvas);
             HDC deviceContext = BeginPaint(window, &paintStruct); // this will give us a handle so we can paint in the window
             LONG _x = paintStruct.rcPaint.left;
             LONG _y = paintStruct.rcPaint.top;
             LONG _width = paintStruct.rcPaint.right - paintStruct.rcPaint.left;
             LONG _height = paintStruct.rcPaint.bottom - paintStruct.rcPaint.top;
-            win32UpdateWindow(deviceContext, _x, _y, _width, _height);
+            win32UpdateWindow(deviceContext, &canvas, _x, _y, _width, _height);
             EndPaint(window, &paintStruct);
         } break;
 
@@ -144,20 +186,48 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR commandLine
         if(windowHandle){
 
             g_appRunning = true;
+            float animationOffset = 0.0f;
+            float offsetValue = 0.0050f;
             // we read the messages sent by kernel or other apps to our window
             // these messages are stored in a message queue
             MSG messageFromWInKernel;
             while (g_appRunning)
             {
-                BOOL messageResult = GetMessage(&messageFromWInKernel, 0, 0, 0);
-                if(messageResult > 0){
+                // PeekMessage does not block when there are not messages!
+                while(PeekMessage(&messageFromWInKernel, 0, 0, 0, PM_REMOVE)){
+                    if(messageFromWInKernel.message == WM_QUIT){
+                        g_appRunning = false;
+                    }
                     TranslateMessage(&messageFromWInKernel); // more windows crap! 
-                    DispatchMessage(&messageFromWInKernel); // again, more more windows crap!  
+                    DispatchMessage(&messageFromWInKernel); // again, more more windows crap!
                 }
-                else{
-                    OutputDebugString("Window is closed");
-                    break;
+
+                // game loop apparently!
+                animationOffset += offsetValue;
+                if(animationOffset > 1.0){
+                    animationOffset = 1.0f;
+                    offsetValue = -offsetValue;
                 }
+                if(animationOffset < 0.0f){
+                    animationOffset = 0.0f;
+                    offsetValue = -offsetValue;
+                }
+                
+
+
+                renderGradient(animationOffset);
+
+                HDC deviceContext = GetDC(windowHandle);
+                RECT windowRect;
+                GetClientRect(windowHandle, &windowRect);
+                int windowWidth = windowRect.right - windowRect.left;
+                int windowHeight = windowRect.bottom - windowRect.top;
+                win32UpdateWindow(deviceContext, &windowRect, 0, 0, windowWidth, windowHeight);
+                ReleaseDC(windowHandle, deviceContext);
+
+                
+
+
            }
         } 
         else{
